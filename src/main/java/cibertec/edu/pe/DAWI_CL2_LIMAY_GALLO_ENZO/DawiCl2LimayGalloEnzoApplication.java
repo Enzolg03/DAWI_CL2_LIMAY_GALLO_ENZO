295e38fc0cdead7f53437d36b793579a87083591
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2023-10-29 10:41:54 -0500

    first commit

---
+++ src/main/java/cibertec/edu/pe/DAWI_CL2_LIMAY_GALLO_ENZO/DawiCl2LimayGalloEnzoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL2_LIMAY_GALLO_ENZO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl2LimayGalloEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl2LimayGalloEnzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DAWI_CL2_LIMAY_GALLO_ENZO/DawiCl2LimayGalloEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL2_LIMAY_GALLO_ENZO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl2LimayGalloEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
